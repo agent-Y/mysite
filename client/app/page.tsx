e6b5fedf22a1a53024e4428217c381530c4acbdc
agent-Y <yuki.hashimoto.0513@gmail.com>
2024-10-21 23:49:08 +0900

    commit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Works from "@/components/Works";
@@ -8,7 +7,7 @@ import Contact from "@/components/Contact";
 import Skill from "@/components/layout/Skill";
 import { useInView } from "react-intersection-observer";
 import Navigation from "@/components/layout/Navigation";
-import TabContext from "@/context/TabContext";
+
 import Apps from "@/components/Apps";
 
 const sections = [
@@ -21,29 +20,12 @@ const sections = [
 ];
 
 export default function Home() {
-  const router = useRouter();
-  const [currentTab, setCurrentTab] = useState("");
-
-  // フックをトップレベルで呼び出し
-  // const inViewRefs = sections.map(() => useInView({ threshold: 1 }));
-
-  // useEffect(() => {
-  //   const index = inViewRefs.findIndex((ref) => ref[1].inView); // ref[1]がinViewの状態
-  //   if (index >= 0) {
-  //     const newTab = sections[index].id;
-  //     setCurrentTab(newTab);
-  //     router.push(`/#${newTab}`);
-  //   }
-  // }, [inViewRefs, router]); // routerを依存配列に追加
-
   return (
-    // <TabContext.Provider value={{ currentTab }}>
     <div className="relative h-screen w-screen">
       <Navigation />
       {sections.map((section, index) => (
         <div
           key={section.id}
-          // ref={inViewRefs[index][0]} // ref[0]が要素の参照
           id={section.id}
           className="shrink-0 snap-always snap-start h-screen relative"
         >
@@ -51,6 +33,5 @@ export default function Home() {
         </div>
       ))}
     </div>
-    // </TabContext.Provider>
   );
 }
--- client/components/About.tsx
+++ client/components/About.tsx
@@ -2,75 +2,75 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { AiFillGithub } from "react-icons/ai";
 import Link from "next/link";
 
 export const metadata: Metadata = {
- title: "Contact - yuki hashimoto",
+  title: "Contact - yuki hashimoto",
 };
 
 export default function About() {
- return (
-  <div className=" p-4 h-screen w-screen flex items-center">
-   <div className="mx-auto max-w-6xl w-full ">
-    <motion.h1
-     whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-     className="text-6xl font-bold text-white pb-4"
-    >
-     About
-    </motion.h1>
-    <motion.div
-     initial={{ opacity: 0 }}
-     whileInView={{ opacity: 1 }}
-     transition={{ delay: 0.5, duration: 1 }}
-     className="max-w-2xl mx-auto flex flex-col gap-8"
-    >
-     <div className="rounded-full flex justify-center md:justify-start ">
-      <Image
-       src="/images/profile.png"
-       width={100}
-       height={100}
-       alt="profile image"
-       className="rounded-full h-18 w-18 object-cover"
-      />
-     </div>
-     <div className="">
-      <p className="text-sm md:text-base text-white font-light whitespace-pre-line first-letter:text-2xl first-letter:font-extrabold">
-       {`My name is Yuki. I am a freelance full-stack developer based in Osaka with a passion for building digital services and products. I have a knack for all things related to launching products, from planning and designing to solving real-life problems with code. 
+  return (
+    <div className=" p-4 h-screen w-screen flex items-center">
+      <div className="mx-auto max-w-6xl w-full ">
+        <motion.h1
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          className="text-6xl font-bold text-white pb-4"
+        >
+          About
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
+          className="max-w-2xl mx-auto flex flex-col gap-8"
+        >
+          <div className="rounded-full flex justify-center md:justify-start ">
+            <Image
+              src="/images/profile.jpg"
+              width={100}
+              height={100}
+              alt="profile image"
+              className="rounded-full h-18 w-18 object-cover"
+            />
+          </div>
+          <div className="">
+            <p className="text-sm md:text-base text-white font-light whitespace-pre-line first-letter:text-2xl first-letter:font-extrabold">
+              {`My name is Yuki. I am a freelance full-stack developer based in Osaka with a passion for building digital services and products. I have a knack for all things related to launching products, from planning and designing to solving real-life problems with code.
        I started studying programming to automate inventory management for a used clothing sales business I was involved in during my university days. I even developed and sold tools for that
        purpose. When I'm not online, I enjoy making my own music and DJing.`}
-      </p>
-     </div>
-     <div className="flex flex-col gap-4">
-      <motion.div
-       initial={{ opacity: 0, x: -10 }}
-       whileInView={{ opacity: 1, x: 0 }}
-       transition={{ delay: 0.7, duration: 1 }}
-      >
-       <h2 className="text-xl text-white font-semibold">On the Web</h2>
-       <Link
-        target="_blank"
-        href="https://github.com/agent-y"
-        className="text-white flex items-center pl-8 gap-2"
-       >
-        <AiFillGithub className="md:h-10 md:w-10 h-6 w-6" />
-        <span className="text-sm md:text-base">Github</span>
-       </Link>
-      </motion.div>
-      <motion.div
-       initial={{ opacity: 0, x: -10 }}
-       whileInView={{ opacity: 1, x: 0 }}
-       transition={{ delay: 0.9, duration: 1 }}
-      >
-       <h2 className="text-xl text-white font-semibold">Likes</h2>
-       <p className="text-white text-sm pl-8">
-        DTM, Playing Tennis, Traveling, Camping
-       </p>
-      </motion.div>
-     </div>
-    </motion.div>
-   </div>
-  </div>
- );
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <motion.div
+              initial={{ opacity: 0, x: -10 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.7, duration: 1 }}
+            >
+              <h2 className="text-xl text-white font-semibold">On the Web</h2>
+              <Link
+                target="_blank"
+                href="https://github.com/agent-y"
+                className="text-white flex items-center pl-8 gap-2"
+              >
+                <AiFillGithub className="md:h-10 md:w-10 h-6 w-6" />
+                <span className="text-sm md:text-base">Github</span>
+              </Link>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -10 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.9, duration: 1 }}
+            >
+              <h2 className="text-xl text-white font-semibold">Likes</h2>
+              <p className="text-white text-sm pl-8">
+                Tennis, Fishing, Traveling, Camping, DTM
+              </p>
+            </motion.div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
 }
